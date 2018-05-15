b3fd5c4b3c1aedf44e4129da2ba35534ba9e5064
Hmvp <hmvp@users.noreply.github.com>
2018-05-15 13:28:07 +0200

    Fix review comments

--- src/printer.rs
+++ src/printer.rs
@@ -11,7 +11,7 @@ pub struct Printer {
 }
 
 impl Visitor for Printer {
-    fn visit(&self, tree: &Tree, path: &[(usize, usize)], _: &[&str]) {
+    fn visit(&self, tree: &Tree, path: &[(usize, usize)], _parents: &[&str]) {
         // The "colored" crate doesn't support coloring for Windows:
         let colored = if cfg!(target_os = "windows") {
             false
