b92f0b88233cb67f0757c91c1d509c40907a4a06
Atamert Ölçgen <muhuk@muhuk.com>
2019-03-16 11:26:44 +0800

    Remove unnecessary closure (clippy)

--- src/builder.rs
+++ src/builder.rs
@@ -1,4 +1,4 @@
-use std::{fs, io, path};
+use std::{fs, io, path, string::ToString};
 
 use syntax::print::pprust;
 use syntax::{ast, source_map, visit};
@@ -57,7 +57,8 @@ impl<'a> Builder<'a> {
             entry
                 .path()
                 .file_stem()
-                .and_then(|s| s.to_str().map(|s| s.to_string()))
+                .and_then(|s| s.to_str())
+                .map(ToString::to_string)
         }
         Ok(try!(fs::read_dir(&dir_path))
             .filter_map(|e| e.ok())
