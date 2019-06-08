61490c1f50568ea7d25f8818b4e178be606d9288
Hiram van Paassen <hiramvanpaassen@dutchcodersnetwork.nl>
2019-06-08 13:03:50 +0200

    Implement Display for Error and use that for errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use json;
+use std::fmt;
 use std::io;
 
 #[derive(Debug)]
@@ -10,4 +11,25 @@ pub enum Error {
     NoTargetProvided,
     NotACargoFolder,
     Syntax(String),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::CargoExecutionFailed(error) => {
+                write!(f, "Failed to run `cargo` command.\n{:?}", error)
+            }
+            Error::InvalidManifestJson(error) => {
+                write!(f, "Failed to parse JSON response.\n{:?}", error)
+            }
+            Error::NoLibraryTargetFound => write!(f, "No library target found."),
+            Error::NoMatchingBinaryTargetFound => write!(f, "No matching binary target found."),
+            Error::NoTargetProvided => write!(f, "Please specify a target to process."),
+            Error::NotACargoFolder => write!(
+                f,
+                "could not find `Cargo.toml` in `/home/hiram/git` or any parent directory"
+            ),
+            Error::Syntax(error) => write!(f, "Failed to parse: {}", error),
+        }
+    }
 }
