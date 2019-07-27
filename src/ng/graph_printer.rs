fba2ffcb071b312fa4d1390f2ab679e3fc8fe91b
Atamert Ölçgen <muhuk@muhuk.com>
2019-07-27 12:49:59 +0800

    Add nodes to the output

--- src/ng/graph_printer.rs
+++ src/ng/graph_printer.rs
@@ -1,11 +1,15 @@
 use error::Error;
-use ng::graph::{Graph, Module};
+use ng::graph::{Graph, Module, Visibility};
+use std::iter::repeat;
 
 pub fn print(graph: &Graph, include_orphans: bool) -> Result<(), Error> {
+    let mut indent: usize = 0;
     let root_node: Module = find_root_module(&graph)?;
 
     println!("digraph {{\n\tlabel=\"{}\";", root_node.name());
-    // TODO: rest of the graph comes here.
+    indent += 4;
+    print_nodes(&graph, include_orphans, indent)?;
+
     println!("}}");
     Ok(())
 }
@@ -17,4 +21,24 @@ fn find_root_module(graph: &Graph) -> Result<Module, Error> {
         (Some(module), 0) => Ok(module),
         (Some(_), _) => Err(Error::Graph("There are multiple root modules.".to_owned())),
     }
+}
+
+fn print_nodes(graph: &Graph, include_orphans: bool, indent: usize) -> Result<(), Error> {
+    let indent_str: String = repeat(' ').take(indent).collect();
+    for module in graph.nodes() {
+        let node_color: String = (match module.visibility() {
+            Some(Visibility::Public) => "green",
+            Some(Visibility::Private) => "gold",
+            None => "red",
+        })
+        .to_owned();
+        println!(
+            "{}\"{}\" [label=\"{}\", color={}]",
+            indent_str,
+            module.path(),
+            module.name(),
+            node_color
+        );
+    }
+    Ok(())
 }
