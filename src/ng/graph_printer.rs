19fa2d68f583c3703484f914669b2e8ceb3b7e3d
Atamert Ölçgen <muhuk@muhuk.com>
2019-07-27 16:22:43 +0800

    Refactor & simplify graph rendering

--- src/ng/graph_printer.rs
+++ src/ng/graph_printer.rs
@@ -1,22 +1,28 @@
 use error::Error;
 use ng::graph::{Edge, Graph, Module, Visibility};
-use std::iter::repeat;
 
 pub fn print(graph: &Graph, include_orphans: bool) -> Result<(), Error> {
-    let mut indent: usize = 0;
+    let indent_str: &str = "    ";
     let root_node: Module = find_root_module(&graph)?;
 
-    println!("digraph {{\n\tlabel=\"{}\";", root_node.name());
-    indent += 4;
-    print_nodes(
-        graph.nodes().filter(|m| {
-            // All modules if include_orhans is true,
-            // else only non-orphaned modules.
-            include_orphans || !m.is_orphan()
-        }),
-        indent,
-    )?;
-    print_edges(graph.all_edges(), indent)?;
+    println!("digraph {{\n{}label=\"{}\";", indent_str, root_node.name());
+
+    println!("{}// Modules", indent_str);
+    for module in graph.nodes().filter(|m| {
+        // All modules if include_orhans is true,
+        // else only non-orphaned modules.
+        include_orphans || !m.is_orphan()
+    }) {
+        print!("{}", indent_str);
+        print_node(module);
+    }
+
+    println!("{}// Edges", indent_str);
+    for (from, to, edge) in graph.all_edges() {
+        print!("{}", indent_str);
+        print_edge(from, to, edge);
+    }
+
     println!("}}");
     Ok(())
 }
@@ -30,47 +36,25 @@ fn find_root_module(graph: &Graph) -> Result<Module, Error> {
     }
 }
 
-fn print_edges<'a, I>(edges: I, indent: usize) -> Result<(), Error>
-where
-    I: Iterator<Item = (Module, Module, &'a Edge)>,
-{
-    let indent_str: String = repeat(' ').take(indent).collect();
-    for (from, to, edge) in edges {
-        let edge_style: &str = match edge {
-            Edge::Child => "[weight=100, color=azure4]",
-            Edge::Dependency(_) => "[weight=90, color=darkviolet]",
-            Edge::Unconnected => "[weight=50, color=azure2]",
-        };
-        println!(
-            "{}\"{}\" -> \"{}\" {}",
-            indent_str,
-            from.path(),
-            to.path(),
-            edge_style
-        );
-    }
-    Ok(())
+fn print_edge(from: Module, to: Module, edge: &Edge) {
+    let edge_style: &str = match edge {
+        Edge::Child => "[weight=100, color=azure4]",
+        Edge::Dependency(_) => "[weight=90, color=darkviolet]",
+        Edge::Unconnected => "[weight=50, color=azure2]",
+    };
+    println!("\"{}\" -> \"{}\" {}", from.path(), to.path(), edge_style);
 }
 
-fn print_nodes<I>(nodes: I, indent: usize) -> Result<(), Error>
-where
-    I: Iterator<Item = Module>,
-{
-    let indent_str: String = repeat(' ').take(indent).collect();
-    println!("{}// Modules", indent_str);
-    for module in nodes {
-        let node_color: &str = match module.visibility() {
-            Some(Visibility::Public) => "green",
-            Some(Visibility::Private) => "gold",
-            None => "red", // Module is orphaned
-        };
-        println!(
-            "{}\"{}\" [label=\"{}\", color={}]",
-            indent_str,
-            module.path(),
-            module.name(),
-            node_color
-        );
-    }
-    Ok(())
+fn print_node(module: Module) {
+    let node_color: &str = match module.visibility() {
+        Some(Visibility::Public) => "green",
+        Some(Visibility::Private) => "gold",
+        None => "red", // Module is orphaned
+    };
+    println!(
+        "\"{}\" [label=\"{}\", color={}]",
+        module.path(),
+        module.name(),
+        node_color
+    );
 }
