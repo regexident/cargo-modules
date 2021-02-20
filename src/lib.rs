86ac46050f82cac77e6c1dad24ac3eb88a633e42
Vincent Esche <regexident@gmail.com>
2021-02-20 15:28:32 +0100

    Added hidden optional positional dummy argument to consume redundant `modules` argument when executing via `cargo modules`

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,9 @@
-use structopt::StructOpt;
-
+pub mod commands;
 pub mod generate;
+pub mod options;
+
 pub(crate) mod graph;
-pub(crate) mod options;
 pub(crate) mod orphans;
 pub(crate) mod printer;
 pub(crate) mod runner;
 pub(crate) mod theme;
-
-#[structopt(
-    name = "cargo-modules",
-    about = "Print a crate's module tree or graph.",
-    // after_help = r#"
-    // If neither `--bin` nor `--example` are given,
-    // then if the project only has one bin target it will be run.
-    // Otherwise `--bin` specifies the bin target to run.
-    // At most one `--bin` can be provided.
-    // "#
-)]
-#[derive(StructOpt, Clone, PartialEq, Debug)]
-pub enum Command {
-    #[structopt(
-        name = "generate",
-        about = "Generate a visualization for a crate's structure."
-    )]
-    Generate(generate::Command),
-}
-
-impl Command {
-    pub fn run(&self) -> Result<(), anyhow::Error> {
-        match &self {
-            Self::Generate(cmd) => cmd.run(),
-        }
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,20 @@ use log::debug;
 use structopt::StructOpt;
 use yansi::Paint;
 
-use cargo_modules::Command;
+use cargo_modules::options::Options;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+
+    let args: Vec<_> = env::args().collect();
+    debug!("Arguments: {:?}", args);
 
     if env::var("NO_COLOR").is_ok() {
         debug!("Disabling color output");
         Paint::disable()
     }
 
-    let cmd = Command::from_args();
+    let options = Options::from_args();
 
-    cmd.run()
+    options.command.run()
 }
