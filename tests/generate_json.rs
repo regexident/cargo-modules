27c352e196ef74bb56b139b1396ef7fa7dbd5d41
Grayson Hay <grayson@codesee.io>
2021-09-16 11:24:06 -0700

    test: Adds tests for json output based on the tree tests

---
+++ tests/generate_json.rs
@@ -0,0 +1,325 @@
+#[macro_use]
+mod util;
+
+mod colors {
+    mod plain {
+        test_cmd!(
+            args: "generate json",
+            success: true,
+            color_mode: ColorMode::Plain,
+            project: smoke
+        );
+    }
+}
+
+mod default {
+    mod pass {
+        test_cmds!(
+            args: "generate json",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                package_lib_target,
+                virtual_workspace_single_package_lib_target,
+                virtual_workspace_single_package_bin_target,
+                workspace_single_package_lib_target,
+                workspace_single_package_bin_target,
+            ]
+        );
+    }
+
+    mod fail {
+        test_cmds!(
+            args: "generate json",
+            success: false,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_multi_target,
+                virtual_workspace_single_package_multi_target,
+                workspace_single_package_multi_target,
+            ]
+        );
+    }
+}
+
+mod lib {
+    mod pass {
+        test_cmds!(
+            args: "generate json \
+                    --lib",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_lib_target,
+                package_multi_target,
+                virtual_workspace_single_package_lib_target,
+                virtual_workspace_single_package_multi_target,
+                workspace_single_package_lib_target,
+                workspace_single_package_multi_target,
+            ]
+        );
+    }
+
+    mod fail {
+        test_cmds!(
+            args: "generate json \
+                    --lib", // does not exist
+            success: false,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                virtual_workspace_single_package_bin_target,
+                workspace_single_package_bin_target,
+            ]
+        );
+    }
+}
+
+mod bin {
+    mod pass {
+        test_cmds!(
+            args: "generate json \
+                    --bin package_bin_target",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                virtual_workspace_single_package_bin_target,
+                workspace_single_package_bin_target,
+            ]
+        );
+
+        test_cmds!(
+            args: "generate json \
+                    --bin package_multi_target",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_multi_target,
+                virtual_workspace_single_package_multi_target,
+                workspace_single_package_multi_target,
+            ]
+        );
+    }
+
+    mod fail {
+        test_cmds!(
+            args: "generate json \
+                    --bin foobar", // does not exist
+            success: false,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_lib_target,
+                package_multi_target,
+                virtual_workspace_multi_package,
+                virtual_workspace_single_package_lib_target,
+                virtual_workspace_single_package_multi_target,
+                workspace_multi_package,
+                workspace_single_package_lib_target,
+                workspace_single_package_multi_target,
+            ]
+        );
+    }
+}
+
+mod package {
+    mod pass {
+        test_cmds!(
+            args: "generate json \
+                    --package package_lib_target",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_lib_target,
+                virtual_workspace_single_package_lib_target,
+                workspace_single_package_lib_target,
+            ]
+        );
+
+        test_cmds!(
+            args: "generate json \
+                    --package package_bin_target",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                virtual_workspace_single_package_bin_target,
+                workspace_single_package_bin_target,
+            ]
+        );
+    }
+
+    mod fail {
+        test_cmds!(
+            args: "generate json \
+                    --package foobar",
+            success: false,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                package_lib_target,
+                package_multi_target,
+                virtual_workspace_multi_package,
+                virtual_workspace_single_package_bin_target,
+                virtual_workspace_single_package_lib_target,
+                virtual_workspace_single_package_multi_target,
+                workspace_multi_package,
+                workspace_single_package_bin_target,
+                workspace_single_package_lib_target,
+                workspace_single_package_multi_target,
+            ]
+        );
+    }
+}
+
+mod package_lib {
+    mod pass {
+        test_cmds!(
+            args: "generate json \
+                    --package package_lib_target \
+                    --lib",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_lib_target,
+                workspace_single_package_lib_target,
+                virtual_workspace_single_package_lib_target,
+            ]
+        );
+
+        test_cmds!(
+            args: "generate json \
+                    --package package_multi_target \
+                    --lib",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_multi_target,
+                workspace_single_package_multi_target,
+                virtual_workspace_single_package_multi_target,
+            ]
+        );
+    }
+
+    mod fail {
+        test_cmds!(
+            args: "generate json \
+                    --package package_bin_target \
+                    --lib", // does not exist
+            success: false,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                workspace_single_package_bin_target,
+                virtual_workspace_single_package_bin_target,
+            ]
+        );
+    }
+}
+
+mod package_bin {
+    mod pass {
+        test_cmds!(
+            args: "generate json \
+                    --package package_bin_target \
+                    --bin package_bin_target",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                workspace_single_package_bin_target,
+                virtual_workspace_single_package_bin_target,
+            ]
+        );
+
+        test_cmds!(
+            args: "generate json \
+                    --package package_multi_target \
+                    --bin package_multi_target",
+            success: true,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_multi_target,
+                workspace_single_package_multi_target,
+                virtual_workspace_single_package_multi_target,
+            ]
+        );
+    }
+
+    mod fail {
+        test_cmds!(
+            args: "generate json \
+                    --package workspace-package \
+                    --bin foobar", // does not exist
+            success: false,
+            color_mode: ColorMode::Plain,
+            projects: [
+                package_bin_target,
+                package_lib_target,
+                package_multi_target,
+                virtual_workspace_multi_package,
+                virtual_workspace_single_package_bin_target,
+                virtual_workspace_single_package_lib_target,
+                virtual_workspace_single_package_multi_target,
+                workspace_multi_package,
+                workspace_single_package_bin_target,
+                workspace_single_package_lib_target,
+                workspace_single_package_multi_target,
+            ]
+        );
+    }
+}
+
+mod with_orphans {
+    test_cmd!(
+        args: "generate json \
+                --with-orphans",
+        success: true,
+        color_mode: ColorMode::Plain,
+        project: smoke
+    );
+}
+
+mod with_tests {
+    test_cmd!(
+        args: "generate json \
+                --with-tests",
+        success: true,
+        color_mode: ColorMode::Plain,
+        project: smoke
+    );
+}
+
+mod with_types {
+    test_cmd!(
+        args: "generate json \
+                --with-types",
+        success: true,
+        color_mode: ColorMode::Plain,
+        project: smoke
+    );
+}
+
+mod github_issue_80 {
+    mod with_tests {
+        test_cmd!(
+            args: "generate json \
+                    --with-types \
+                    --with-tests",
+            success: true,
+            color_mode: ColorMode::Plain,
+            project: github_issue_80
+        );
+    }
+
+    mod without_tests {
+        test_cmd!(
+            args: "generate json \
+                    --with-types",
+            success: true,
+            color_mode: ColorMode::Plain,
+            project: github_issue_80
+        );
+    }
+}
