7590de7309e37cc5a6c9028126b104f988504dd9
Vincent Esche <regexident@gmail.com>
2023-10-08 15:02:52 +0200

    Add `crate::tree::Node`

---
+++ src/tree/node.rs
@@ -0,0 +1,44 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use ra_ap_ide_db::RootDatabase;
+
+use crate::item::Item;
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Node {
+    pub item: Item,
+    pub subnodes: Vec<Node>,
+}
+
+impl Node {
+    pub fn new(item: Item) -> Self {
+        Self {
+            item,
+            subnodes: vec![],
+        }
+    }
+
+    pub fn push_subnode(&mut self, subnode: Node) {
+        self.subnodes.push(subnode);
+    }
+}
+
+impl Node {
+    pub fn display_name(&self) -> String {
+        self.item.display_name()
+    }
+
+    pub fn display_path(&self) -> String {
+        self.item.display_path()
+    }
+
+    pub fn crate_display_name(&self) -> String {
+        self.item.crate_display_name()
+    }
+
+    pub fn kind_display_name(&self, db: &RootDatabase) -> Option<String> {
+        self.item.kind_display_name(db)
+    }
+}
