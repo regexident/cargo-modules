17bfe646a1c19b8ece19bca1c20b5981473a847f
Vincent Esche <regexident@gmail.com>
2023-12-05 21:21:08 +0100

    Add test projects for `orphans` command

---
+++ tests/orphans.rs
@@ -0,0 +1,49 @@
+#[macro_use]
+mod util;
+
+mod colors {
+    mod plain {
+        test_cmd!(
+            args: "orphans",
+            success: false,
+            color_mode: ColorMode::Plain,
+            project: orphans
+        );
+    }
+
+    mod ansi {
+        test_cmd!(
+            args: "orphans",
+            success: false,
+            color_mode: ColorMode::Ansi,
+            project: orphans
+        );
+    }
+
+    mod truecolor {
+        test_cmd!(
+            args: "orphans",
+            success: false,
+            color_mode: ColorMode::TrueColor,
+            project: orphans
+        );
+    }
+}
+
+mod no_orphans {
+    test_cmd!(
+        args: "orphans",
+        success: true,
+        color_mode: ColorMode::Plain,
+        project: no_orphans
+    );
+}
+
+mod orphans {
+    test_cmd!(
+        args: "orphans",
+        success: false,
+        color_mode: ColorMode::Plain,
+        project: orphans
+    );
+}
