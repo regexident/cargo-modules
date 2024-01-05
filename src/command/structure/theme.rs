9be0891cc6d2e9ac1fd5dca6d0d5e17fa6ecda52
Vincent Esche <regexident@gmail.com>
2024-01-05 12:43:44 +0100

    Fix clippy warnings

--- src/command/structure/theme.rs
+++ src/command/structure/theme.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pub use yansi::{Color, Style};
+pub use yansi::Style;
 
 use crate::colors::cli::color_palette;
 
